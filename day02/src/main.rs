fc353b938ef2ea1755f88dd864d7cc3de4ac86a8
Mathieu Lemay <acidrain1@gmail.com>
2024-12-02 08:48:46 -0500

    [skip ci] WIP: d2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -36,7 +36,14 @@ mod tests {
 
     #[fixture]
     fn test_input() -> Vec<String> {
-        parse_test_input("")
+        parse_test_input("
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+        ")
     }
 
     #[fixture]
