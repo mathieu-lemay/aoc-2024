bc17f76a2ac2cd9bad38c9e09fa697400bd886bf
Mathieu Lemay <acidrain1@gmail.com>
2024-12-06 01:27:52 -0500

    Day 06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::time::Instant;
 
-use aoc_common::{format_duration, get_input, tracing_init};
+use aoc_common::{format_duration, get_input, tracing_init, Point};
 
 fn main() {
     tracing_init();
@@ -10,7 +11,7 @@ fn main() {
 
     let start = Instant::now();
 
-    let (r1, r2) = solve(input.as_slice());
+    let (r1, r2) = solve(&input);
 
     let t = start.elapsed().as_nanos();
 
@@ -20,11 +21,200 @@ fn main() {
 }
 
 #[tracing::instrument(skip_all)]
-fn solve(_input: &[String]) -> (impl Display, impl Display) {
-    let p1 = 0;
-    let p2 = 0;
+fn solve(input: &Vec<String>) -> (impl Display, impl Display) {
+    let mut map: Map = input.into();
+    let p1 = map.move_until_oob();
+
+    let map: Map = input.into();
+    let p2 = map.find_loops();
 
     (p1, p2)
+}
+
+type Position = Point<usize>;
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Map {
+    width: usize,
+    height: usize,
+    guard_pos: Position,
+    guard_dir: Direction,
+    obstacles: Vec<Position>,
+}
+
+impl Default for Map {
+    fn default() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            guard_pos: Point::new(0, 0),
+            guard_dir: Direction::Up,
+            obstacles: Vec::new(),
+        }
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for x in 0..self.height {
+            for y in 0..self.width {
+                let p = Point::new(x, y);
+                if p == self.guard_pos {
+                    write!(f, "^")?;
+                } else if self.obstacles.contains(&p) {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl From<&Vec<String>> for Map {
+    fn from(value: &Vec<String>) -> Self {
+        let mut guard_pos = None;
+        let mut obstacles = Vec::new();
+
+        for (x, row) in value.iter().enumerate() {
+            for (y, p) in row.chars().enumerate() {
+                match p {
+                    '^' => guard_pos = Some(Point::new(x, y)),
+                    '#' => obstacles.push(Point::new(x, y)),
+                    _ => {}
+                }
+            }
+        }
+
+        Self {
+            width: value[0].len(),
+            height: value.len(),
+            guard_pos: guard_pos.unwrap(),
+            obstacles,
+            ..Default::default()
+        }
+    }
+}
+
+impl Map {
+    fn move_until_oob(&mut self) -> usize {
+        let mut visited = Vec::new();
+
+        visited.push((self.guard_pos, self.guard_dir));
+
+        loop {
+            if let Err(()) = self.move_guard() {
+                break;
+            }
+
+            visited.push((self.guard_pos, self.guard_dir));
+        }
+
+        let positions: HashSet<Position> = visited.iter().map(|(p, _)| *p).collect();
+        positions.len()
+    }
+
+    fn find_loops(&self) -> usize {
+        let mut count = 0;
+
+        for x in 0..self.height {
+            for y in 0..self.width {
+                println!(
+                    "Testing x={}, y={}, h={}, w={}",
+                    x, y, self.height, self.width
+                );
+
+                let p = Point::new(x, y);
+                if p == self.guard_pos || self.obstacles.contains(&p) {
+                    continue;
+                }
+
+                let mut map = self.clone();
+                map.obstacles.push(p);
+
+                let mut visited = Vec::new();
+                visited.push((map.guard_pos, map.guard_dir));
+
+                loop {
+                    if let Err(()) = map.move_guard() {
+                        break;
+                    }
+
+                    if visited.contains(&(map.guard_pos, map.guard_dir)) {
+                        count += 1;
+                        break;
+                    }
+
+                    visited.push((map.guard_pos, map.guard_dir));
+                }
+            }
+        }
+
+        count
+    }
+
+    fn move_guard(&mut self) -> Result<(), ()> {
+        let next = match self.guard_dir {
+            Direction::Up => {
+                if self.guard_pos.x == 0 {
+                    return Err(());
+                }
+
+                Point::new(self.guard_pos.x - 1, self.guard_pos.y)
+            }
+            Direction::Down => {
+                if self.guard_pos.x == self.height - 1 {
+                    return Err(());
+                }
+
+                Point::new(self.guard_pos.x + 1, self.guard_pos.y)
+            }
+            Direction::Left => {
+                if self.guard_pos.y == 0 {
+                    return Err(());
+                }
+
+                Point::new(self.guard_pos.x, self.guard_pos.y - 1)
+            }
+            Direction::Right => {
+                if self.guard_pos.y == self.width - 1 {
+                    return Err(());
+                }
+
+                Point::new(self.guard_pos.x, self.guard_pos.y + 1)
+            }
+        };
+
+        if self.obstacles.contains(&next) {
+            self.guard_dir = self.guard_dir.turn_right();
+        } else {
+            self.guard_pos = next;
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -36,7 +226,20 @@ mod tests {
 
     #[fixture]
     fn test_input() -> Vec<String> {
-        parse_test_input("")
+        parse_test_input(
+            "
+            ....#.....
+            .........#
+            ..........
+            ..#.......
+            .......#..
+            ..........
+            .#..^.....
+            ........#.
+            #.........
+            ......#...
+        ",
+        )
     }
 
     #[fixture]
@@ -46,29 +249,40 @@ mod tests {
 
     #[rstest]
     fn test_p1(test_input: Vec<String>) {
-        let res = 0;
+        let mut map: Map = (&test_input).into();
+        println!("{}", map);
 
-        assert_eq!(res, 1);
+        let res = map.move_until_oob();
+
+        println!("{}", &map);
+
+        assert_eq!(res, 41);
     }
 
     #[rstest]
     fn test_p1_full_input(puzzle_input: Vec<String>) {
-        let res = 0;
+        let mut map: Map = (&puzzle_input).into();
 
-        assert_eq!(res, 1);
+        let res = map.move_until_oob();
+
+        assert_eq!(res, 4826);
     }
 
     #[rstest]
     fn test_p2(test_input: Vec<String>) {
-        let res = 0;
+        let map: Map = (&test_input).into();
 
-        assert_eq!(res, 1);
+        let res = map.find_loops();
+
+        assert_eq!(res, 6);
     }
 
     #[rstest]
     fn test_p2_full_input(puzzle_input: Vec<String>) {
-        let res = 0;
+        let map: Map = (&puzzle_input).into();
 
-        assert_eq!(res, 1);
+        let res = map.find_loops();
+
+        assert_eq!(res, 4826);
     }
 }
