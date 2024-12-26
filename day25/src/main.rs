36eaa523076cb86ca15148c7fa6211233b6a2f7a
Mathieu Lemay <acidrain1@gmail.com>
2024-12-26 10:36:09 -0500

    Day 25, Part 1

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 use std::time::Instant;
 
 use aoc_common::{format_duration, get_input, tracing_init};
+use itertools::Itertools;
 
 fn main() {
     tracing_init();
@@ -20,11 +21,63 @@ fn main() {
 }
 
 #[tracing::instrument(skip_all)]
-fn solve(_input: &[String]) -> (impl Display, impl Display) {
-    let p1 = 0;
+fn solve(input: &[String]) -> (impl Display, impl Display) {
+    let schematics = Schematics::from(input);
+    let p1 = schematics.get_arrangements();
     let p2 = 0;
 
     (p1, p2)
+}
+
+#[derive(Debug)]
+struct Schematics {
+    locks: Vec<Vec<u8>>,
+    keys: Vec<Vec<u8>>,
+}
+
+impl Schematics {
+    #[tracing::instrument(skip_all)]
+    fn get_arrangements(&self) -> usize {
+        self.keys
+            .iter()
+            .cartesian_product(&self.locks)
+            .filter(|(k, l)| k.iter().zip(l.iter()).all(|(a, b)| a + b <= 5))
+            .count()
+    }
+}
+
+impl From<&[String]> for Schematics {
+    #[tracing::instrument(skip_all)]
+    fn from(values: &[String]) -> Self {
+        let mut locks = Vec::new();
+        let mut keys = Vec::new();
+
+        for chunk in values.split(|s| s.is_empty()) {
+            let heights: Vec<u8> = (0usize..5)
+                .map(|i| {
+                    (chunk
+                        .iter()
+                        .filter_map(|s| {
+                            if let Some("#") = s.get(i..=i) {
+                                Some(())
+                            } else {
+                                None
+                            }
+                        })
+                        .count()
+                        - 1) as u8
+                })
+                .collect();
+
+            if chunk[0] == "#####" {
+                locks.push(heights);
+            } else {
+                keys.push(heights);
+            }
+        }
+
+        Self { locks, keys }
+    }
 }
 
 #[cfg(test)]
@@ -36,7 +89,49 @@ mod tests {
 
     #[fixture]
     fn test_input() -> Vec<String> {
-        parse_test_input("")
+        parse_test_input(
+            "
+            #####
+            .####
+            .####
+            .####
+            .#.#.
+            .#...
+            .....
+
+            #####
+            ##.##
+            .#.##
+            ...##
+            ...#.
+            ...#.
+            .....
+
+            .....
+            #....
+            #....
+            #...#
+            #.#.#
+            #.###
+            #####
+
+            .....
+            .....
+            #.#..
+            ###..
+            ###.#
+            ###.#
+            #####
+
+            .....
+            .....
+            .....
+            #....
+            #.#..
+            #.#.#
+            #####
+        ",
+        )
     }
 
     #[fixture]
@@ -46,16 +141,18 @@ mod tests {
 
     #[rstest]
     fn test_p1(test_input: Vec<String>) {
-        let res = 0;
+        let schematics = Schematics::from(test_input.as_slice());
+        let res = schematics.get_arrangements();
 
-        assert_eq!(res, 1);
+        assert_eq!(res, 3);
     }
 
     #[rstest]
     fn test_p1_full_input(puzzle_input: Vec<String>) {
-        let res = 0;
+        let schematics = Schematics::from(puzzle_input.as_slice());
+        let res = schematics.get_arrangements();
 
-        assert_eq!(res, 1);
+        assert_eq!(res, 3307);
     }
 
     #[rstest]
